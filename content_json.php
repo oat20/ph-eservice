d6745521bfae02ae926360060f9375258b8c6b8a
oat20 <iceeagle99@gmail.com>
2021-02-12 17:54:47 +0700

    Add files via upload

---
+++ content_json.php
@@ -0,0 +1,18 @@
+<?php
+require 'config.php';
+require 'condb-inc.php';
+
+$json = array();
+$sql = mysqli_query($result, "select title,
+    url,
+    icon
+    from jos_weblinks
+    where published = '1'
+    order by convert(title using tis620)
+");
+while($rs = mysqli_fetch_assoc($sql)){
+    array_push($json, $rs);
+}
+print json_encode($json, JSON_UNESCAPED_UNICODE);
+mysqli_close($result);
+?>
